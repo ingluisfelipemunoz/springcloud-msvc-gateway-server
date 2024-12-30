aa5204fc2cea6fe6437cb79a7c1dc69a5ce26336
Luis Felipe <ingluisfelipemunoz@gmail.com>
2024-12-29 22:39:22 -0400

    configuring filter order

--- src/main/java/com/felipe/springcloud/app/gateway/filters/SampleGlobalFilter.java
+++ src/main/java/com/felipe/springcloud/app/gateway/filters/SampleGlobalFilter.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseCookie;
 import org.springframework.stereotype.Component;
@@ -12,7 +13,7 @@ import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
 @Component
-public class SampleGlobalFilter implements GlobalFilter {
+public class SampleGlobalFilter implements GlobalFilter, Ordered {
     private final Logger logger = LoggerFactory.getLogger(SampleGlobalFilter.class);
 
     @Override
@@ -24,6 +25,11 @@ public class SampleGlobalFilter implements GlobalFilter {
             exchange.getResponse().getCookies().add("color", ResponseCookie.from("color", "yellow").build());
             exchange.getResponse().getHeaders().setContentType(MediaType.TEXT_PLAIN);
         }));
+    }
+
+    @Override
+    public int getOrder() {
+        return 100;
     }
 
 }
