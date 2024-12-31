06d462e8e2d4f4a75c69aa170a52c91193b2e17f
Luis Felipe <ingluisfelipemunoz@gmail.com>
2024-12-31 14:24:14 -0400

    creating a personalized gateway filter and providing configurations to it from application.yml

---
+++ src/main/java/com/felipe/springcloud/app/gateway/filters/factory/SampleCookieGatewayFilterFactory.java
@@ -0,0 +1,91 @@
+package com.felipe.springcloud.app.gateway.filters.factory;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.http.ResponseCookie;
+import org.springframework.stereotype.Component;
+
+import reactor.core.publisher.Mono;
+
+@Component
+public class SampleCookieGatewayFilterFactory
+        extends AbstractGatewayFilterFactory<SampleCookieGatewayFilterFactory.ConfigurationCookie> {
+
+    private final Logger logger = LoggerFactory.getLogger(SampleCookieGatewayFilterFactory.class);
+
+    public SampleCookieGatewayFilterFactory() {
+        super(ConfigurationCookie.class);
+    }
+
+    @Override
+    public GatewayFilter apply(ConfigurationCookie config) {
+        return (exchange, chain) -> {
+            logger.info("--EXECUTING PRE-GATEWAY FILTER FACTORY " + config.message);
+            return chain.filter(exchange).then(Mono.fromRunnable(() -> {
+                Optional.ofNullable(config.value).ifPresent((cookie) -> {
+                    exchange.getResponse().addCookie(ResponseCookie.from(config.name, cookie).build());
+                });
+                logger.info("--EXECUTING POST-GATEWAY FILTER FACTORY " + config.message);
+            }));
+        };
+    }
+
+    /*
+     * in case of using the inline/unordered implementation of the Filter
+     * Configuration in the application.yml, use this method to set the order
+     * 
+     * @Override
+     * public List<String> shortcutFieldOrder() {
+     * return Arrays.asList("message", "name", "value");
+     * }
+     */
+
+    /*
+     * with this method we can set the filter name that we are using in the
+     * application.yml, used in case of a personalized name instead of the class
+     * name
+     * 
+     * @Override
+     * public String name() {
+     * return "ExampleCookie";
+     * }
+     */
+
+    public static class ConfigurationCookie {
+        private String name;
+        private String value;
+        private String message;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getValue() {
+            return value;
+        }
+
+        public void setValue(String value) {
+            this.value = value;
+        }
+
+        public String getMessage() {
+            return message;
+        }
+
+        public void setMessage(String message) {
+            this.message = message;
+        }
+
+    }
+
+}
